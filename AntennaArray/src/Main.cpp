f429a6dd88c4b728a2a2329eb6015c32729f93e6
unknown <john.berg.96bits@gmail.com>
2019-10-11 22:53:21 +0100

    Need to tidy up solution

--- AntennaArray/src/Main.cpp
+++ AntennaArray/src/Main.cpp
@@ -1,156 +1,99 @@
 #include "AntennaArray.h"
 #include <algorithm>
+#include <numeric>
 #include <iostream>
+#include <iterator>
 #include <random>
+#include <valarray>
 
 static std::minstd_rand rng{};
 
 struct Solution
 {
     double score;
-    std::vector<double> position;
+    std::valarray<double> position;
 };
 
 struct Position
 {
     Solution personalBest;
     Solution current;
-    std::vector<double> velocity;
+    std::valarray<double> velocity;
 };
 
-std::vector<double> generateAAS(std::size_t count, double length);
-
-template<typename ForwardIt>
-void dotProduct(
-    ForwardIt first1, 
-    ForwardIt end1,
-    ForwardIt first2,
-    ForwardIt end2);
+std::valarray<double> generateAAS(std::size_t count, double length);
 
 template<typename ForwardIt>
 void runPSO(AntennaArray& aa, ForwardIt first, ForwardIt last);
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
     AntennaArray aa{3, 90.0};
 
     std::vector<Position> solutions(10);
     std::generate(solutions.begin(), solutions.end(), [&]() -> auto
     {
         auto x = generateAAS(3, 1.5);
-        auto score = aa.evaluate(x);
+
+        std::vector<double> temp{std::begin(x), std::end(x)};
+        temp.emplace_back(1.5);
+
+        auto score = aa.evaluate(temp);
         Solution s{score, x};
-        return Position{s, s, std::vector<double>(3)};
+        return Position{s, s, std::valarray<double>(2)};
     });
 
     runPSO(aa, solutions.begin(), solutions.end());
 }
 
 
-std::vector<double> generateAAS(std::size_t count, double length)
+std::valarray<double> generateAAS(std::size_t count, double length)
 {
     const auto padding = (length - AntennaArray::MIN_SPACING * (count)) / count;
     std::uniform_real_distribution<> dis{0.0, padding};
-    std::vector<double> temp(count);
-    temp.back() = length;
+    std::vector<double> temp(count - 1);
 
     std::accumulate(
-        temp.begin(),
-        temp.end() - 1,
+        std::begin(temp),
+        std::end(temp),
         double{},
-        [&](double base, double& now) {
-            
+        [&](double base, double& now)
+        {
             now = dis(rng) + base;
             return now + AntennaArray::MIN_SPACING;
         });
-    return temp;
+    return std::valarray<double>{temp.data(), count - 1};
 }
 
+std::valarray<double> randomVec(std::uniform_real_distribution<>& dis, std::size_t count)
+{
+    std::vector<double> temp(count - 1);
+    std::generate(temp.begin(), temp.end(),
+        [&]() -> auto {return dis(rng);});
+    return {temp.data(), count - 1};
+}
 
 void update(AntennaArray& aa, Position& pos, Solution const& best)
 {
-    const double inertia {0.5};
-    const double cognitiveInertia{0.5};
-    const double socialInertia{0.5};
-
-    auto& s = pos.current;
+    const double inertia {0.721};
+    const double cognitiveInertia{1.1193};
+    const double socialInertia{ 1.1193 };
     
-    // n * v(t)
-    std::for_each(
-        pos.velocity.begin(),
-        pos.velocity.end(),
-        [=](auto& x){x *= inertia;});
-
     std::uniform_real_distribution<> dis{0.0, 1.0};
 
-    // o1 * r1 * (p(t) - x(t))
-    auto t1 = pos.personalBest.position;
-    {
-        std::vector<double> r1(3);
-        std::generate(
-            r1.begin(),
-            r1.end(),
-            [&]() -> auto {
-                return cognitiveInertia * dis(rng);
-            });
-        std::transform(
-            t1.begin(),
-            t1.end(),
-            pos.current.position.begin(),
-            t1.begin(),
-            std::minus<double>{});
-        std::transform(
-            t1.begin(),
-            t1.end(),
-            r1.begin(),
-            t1.begin(),
-            std::multiplies<double>{});
-    }
-
-    // o2 * r2 * (g(t) - x(t))
-    std::vector<double> r2(3);  
-    auto t2 = best.position;
-    {
-        std::generate(
-            r2.begin(),
-            r2.end(),
-            [&]() -> auto {
-                return socialInertia * dis(rng);
-            });
-        std::transform(
-            t2.begin(),
-            t2.end(),
-            pos.current.position.begin(),
-            t2.begin(),
-            std::minus<double>{});
-        std::transform(
-            t2.begin(),
-            t2.end(),
-            r2.begin(),
-            t2.begin(),
-            std::multiplies<double>{});
-    }
-    
     // Move to the new position..
-    std::transform(
-        pos.current.position.begin(),
-        pos.current.position.end(),
-        pos.velocity.begin(),
-        pos.current.position.begin(),
-        std::plus<double>{});
-    std::transform(
-        pos.current.position.begin(),
-        pos.current.position.end(),
-        t1.begin(),
-        pos.current.position.begin(),
-        std::plus<double>{});
-    std::transform(
-        pos.current.position.begin(),
-        pos.current.position.end(),
-        t1.begin(),
-        pos.current.position.begin(),
-        std::plus<double>{});
-        
+    pos.velocity = inertia * pos.velocity
+        + cognitiveInertia * randomVec(dis, 3) * (pos.personalBest.position - pos.current.position)
+        + socialInertia * randomVec(dis, 3) * (best.position - pos.current.position);
+
+    pos.current.position = pos.current.position + pos.velocity;
+    std::vector<double> temp{
+        std::begin(pos.current.position),
+        std::end(pos.current.position)};
+    temp.emplace_back(1.5);
+
+    pos.current.score = aa.evaluate(temp);
     if(pos.current.score < pos.personalBest.score)
         pos.personalBest = pos.current;
 }
@@ -158,6 +101,7 @@ void update(AntennaArray& aa, Position& pos, Solution const& best)
 template<typename ForwardIt>
 void runPSO(AntennaArray& aa, ForwardIt first, ForwardIt last)
 {
+    double overallBest = std::numeric_limits<double>::max();
     while(1)
     {
         auto it = std::min_element(
@@ -169,8 +113,14 @@ void runPSO(AntennaArray& aa, ForwardIt first, ForwardIt last)
         });
 
         auto globalBest = it->personalBest;
-
-        std::cout << "Best: " << globalBest.score << '\n';
+        if(globalBest.score < overallBest)
+        {
+            overallBest = globalBest.score;
+            std::cout << "Best: " << globalBest.score << " [";
+            std::for_each(std::begin(globalBest.position), std::end(globalBest.position),
+                [](auto p) {std::cout << p << ' ';});
+            std::cout << 1.5 << "]\n";
+        }
         std::for_each(first, last, [&](auto& s)
         {
             update(aa, s, globalBest);
